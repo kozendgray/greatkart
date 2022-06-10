86f8c2d1e58d73f8dccc19a42d38756a7325165e
j <kozendgray@gmail.com>
2022-06-10 11:36:57 -0700

    added allowed host IP quotes

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [greatkart-env.eba-wamimbbh.us-west-2.elasticbeanstalk.com]
+ALLOWED_HOSTS = ['greatkart-env.eba-wamimbbh.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
